d6eb7dceacaa27b70b532391b3047baa398f779c
Vinz <ballutvincent@gmail.com>
2020-11-08 08:56:02 +0100

    link the public folder to the main endpoint

--- api_build/5.ts
+++ api_build/5.ts
@@ -2,6 +2,9 @@ import { Application, Context } from 'https://deno.land/x/abc@v1.2.0/mod.ts';
 
 const app = new Application();
 
+//static files
+app.static("/", "./public");
+
 //routes
 app.get("/", async (ctx: Context) => {
   await ctx.file("./public/index.html");
